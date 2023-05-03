ed4a0be6c67df8d37033cb0d009d6f47304e2d7b
nerdican <dslewiston@gmail.com>
2023-05-03 16:00:22 -0700

    Commit
    Working but no drink field in the portal

--- models/res_partner.py
+++ models/res_partner.py
@@ -6,3 +6,20 @@ class ResPartner(models.Model):
 
     drink = fields.Char(string='Favorite Drink')
     munis_vendor_id = fields.Integer(string='Vendor ID', help='Munis Vendor ID')
+    city = fields.Char(string='City', required=False)
+    country_id = fields.Many2one('res.country', string='Country', required=False)
+
+    @api.model
+    def create(self, vals):
+        if 'city' in vals and not vals['city']:
+            vals.pop('city')
+        if 'country_id' in vals and not vals['country_id']:
+            vals.pop('country_id')
+        return super(ResPartner, self).create(vals)
+
+    def write(self, vals):
+        if 'city' in vals and not vals['city']:
+            vals.pop('city')
+        if 'country_id' in vals and not vals['country_id']:
+            vals.pop('country_id')
+        return super(ResPartner, self).write(vals)
